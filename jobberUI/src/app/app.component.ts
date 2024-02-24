398577d229181115da41882bc16a09d51c7d9d44
Raj Palival <rpalival@stevens.edu>
2024-02-24 04:06:12 -0500

    feature/sidebar-nav

--- jobberUI/src/app/app.component.ts
+++ jobberUI/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 
 @Component({
     selector: 'jobber-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CommonModule, SideNavbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
     title = 'jobberUI';
+
+    sidebarExpanded: boolean = true;
 }
---
+++ jobberUI/src/app/side-navbar/side-navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'jobber-side-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './side-navbar.component.html',
+  styleUrl: './side-navbar.component.scss'
+})
+export class SideNavbarComponent {
+
+    @Input() isExpanded: boolean = false;
+
+    @Output() toggleSidebar: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    handleSidebarToggle = () => this.toggleSidebar.emit(!this.isExpanded);
+
+    username = 'Raj Palival';
+
+}
